4896ff3c366d2c1df7aac1bde02830852f5e736e
Oscar Nzabarinda Mukeshimana <101839576+MainKataVerde@users.noreply.github.com>
2024-12-15 19:03:16 +0100

    Añadimos metodo setSettings y setFeaturesButton con su respectivo javadoc

--- calculadora/src/Engine.java
+++ calculadora/src/Engine.java
@@ -2,7 +2,15 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
+/**
+ * Clase que en la que se va a ejecuta toda la mecanica de la calculadora
+ * 
+ */
 public class Engine {
     // Marco de la ventana
     private JFrame frame;
@@ -39,33 +47,85 @@ public class Engine {
 
     // Almacenar temporalmente ciertos valores
     private int num1, num2, result;
-    private char operation;// Marco de la ventana
+    private char operation;
 
     public Engine() {
+        // Marco de la ventana
         this.frame = new JFrame("Calculadora");
-        JPanel contentPanel = new JPanel();
+        // Panel general que ocupa toda la ventana
+        this.contentPanel = new JPanel();
         // Panel norte que contiene el display
-        JPanel displayPanel = new JPanel();
+        this.displayPanel = new JPanel();
         // Panel sur que contiene los botones
-        JPanel buttonPanel = new JPanel();
+        this.buttonPanel = new JPanel();
         // Display
-        JTextField display = new JTextField();
+        this.display = new JTextField("0");
         // Botones
-        JButton n0 = new JButton();
-        JButton n1 = new JButton();
-        JButton n2 = new JButton();
-        JButton n3 = new JButton();
-        JButton n4 = new JButton();
-        JButton n5 = new JButton();
-        JButton n6 = new JButton();
-        JButton n7 = new JButton();
-        JButton n8 = new JButton();
-        JButton n9 = new JButton();
-        JButton divide = new JButton();
-        JButton multiply = new JButton();
-        JButton subtract = new JButton();
-        JButton add = new JButton();
-        JButton equal = new JButton();
-        JButton reset = new JButton();
+        this.n0 = new JButton("0");
+        this.n1 = new JButton("1");
+        this.n2 = new JButton("2");
+        this.n3 = new JButton("3");
+        this.n4 = new JButton("4");
+        this.n5 = new JButton("5");
+        this.n6 = new JButton("6");
+        this.n7 = new JButton("7");
+        this.n8 = new JButton("8");
+        this.n9 = new JButton("9");
+        this.divide = new JButton("/");
+        this.multiply = new JButton("X");
+        this.subtract = new JButton("-");
+        this.add = new JButton("+");
+        this.equal = new JButton("=");
+        this.reset = new JButton("C");
+        // configuramos la ventana
+        setSettings();
+    }
+
+    /**
+     * Este método establece la configuración principal de todos
+     * los componentes visuales de la ventana
+     */
+    public void setSettings() {
+        // Ponemos los layouts
+        this.contentPanel.setLayout(new BorderLayout());
+        this.displayPanel.setLayout(new FlowLayout());
+        this.buttonPanel.setLayout(new GridLayout(4, 4, 2, 5));
+        // setemaos tamaños
+        this.display.setPreferredSize(new Dimension(800, 150));
+        // añimos panel de texto al display
+        this.displayPanel.add(this.display);
+        // añadimos los botones
+        this.buttonPanel.add(this.n7);
+        this.buttonPanel.add(this.n8);
+        this.buttonPanel.add(this.n9);
+        this.buttonPanel.add(this.multiply);
+        this.buttonPanel.add(this.n4);
+        this.buttonPanel.add(this.n5);
+        this.buttonPanel.add(this.n6);
+        this.buttonPanel.add(this.subtract);
+        this.buttonPanel.add(this.n1);
+        this.buttonPanel.add(this.n2);
+        this.buttonPanel.add(this.n3);
+        this.buttonPanel.add(this.add);
+        this.buttonPanel.add(this.n0);
+        this.buttonPanel.add(this.reset);
+        this.buttonPanel.add(this.equal);
+        this.contentPanel.add(this.display, BorderLayout.NORTH);
+        this.contentPanel.add(this.buttonPanel, BorderLayout.CENTER);
+        this.frame.add(this.contentPanel);
+        this.frame.setVisible(true);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setSize(new Dimension(800, 1000));
+    }
+
+    /**
+     * Dependiendo del boton que pongamos cambiara su estilo
+     * 
+     * @param _button El botón sobre el que se van a cambiar las características
+     * @param _type   De qué tipo es el botón sobre el que se van a cambiar
+     *                las características
+     */
+    public void setFeaturesButton(JButton _button, ButtonType _type) {
+
     }
 }
