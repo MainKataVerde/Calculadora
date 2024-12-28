aea3e30e27a39c5f161068bb515ddb1939ecdffe
Oscar Nzabarinda Mukeshimana <101839576+MainKataVerde@users.noreply.github.com>
2024-12-28 19:03:18 +0100

    Subimos javadoc

--- calculadora/src/Main.java
+++ calculadora/src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
+
+    /**
+     * Metodo main en el que se empieza a ejercutar la logica de la calculadora
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Engine engine = new Engine();
     }
